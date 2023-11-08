2089758e6630f9280271e704224468c63efc2870
neharaut00 <neharaut372@gmail.com>
2023-11-08 20:41:10 +0530

    saving the audio file locally

--- Voice_alert_node/lib/speech-to-text.js
+++ Voice_alert_node/lib/speech-to-text.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws");
 const Transform = require('stream').Transform;
+const fs = require('fs');
 const speech = require("@google-cloud/speech");
 require("dotenv").config();
 const sttStore = require('./stt-store');
@@ -47,26 +48,52 @@ function handleIncomingWSConnection(ws, req) {
         newCall();
         break;
       case "start":
+        let streamSid = msg.streamSid;
         console.log(`Starting Media Stream ${msg.streamSid}`);
         recognizeStream = CallerSpeechClient
           .streamingRecognize(request)
           .on("error", (msg) => { handleSttError(msg); })
           .on("data", (data) => { handleSttData(data); });
+          ws.wstream = fs.createWriteStream(__dirname + `/${Date.now()}.wav`, { encoding: 'binary' });
+          ws.wstream.write(Buffer.from([
+            0x52,0x49,0x46,0x46,0x62,0xb8,0x00,0x00,0x57,0x41,0x56,0x45,0x66,0x6d,0x74,0x20,
+            0x12,0x00,0x00,0x00,0x07,0x00,0x01,0x00,0x40,0x1f,0x00,0x00,0x80,0x3e,0x00,0x00,
+            0x02,0x00,0x04,0x00,0x00,0x00,0x66,0x61,0x63,0x74,0x04,0x00,0x00,0x00,0xc5,0x5b,
+            0x00,0x00,0x64,0x61,0x74,0x61,0x00,0x00,0x00,0x00, // Those last 4 bytes are the data length
+          ]));       
         break;
       case "media":
         recognizeStream.write(msg.media.payload);
+        ws.wstream.write(Buffer.from(msg.media.payload, 'base64'));
         break;
       case "stop":
         console.log(`Call Has Ended`);
         recognizeStream.destroy();
         currentCallEndTime = Date.now();
+        // Now the only thing missing is to write the number of data bytes in the header
+        ws.wstream.write("", () => {
+          let fd = fs.openSync(ws.wstream.path, 'r+'); // `r+` mode is needed in order to write to arbitrary position
+          let count = ws.wstream.bytesWritten;
+          count -= 58; // The header itself is 58 bytes long and we only want the data byte length
+          console.log(count)
+          fs.writeSync(
+            fd,
+            Buffer.from([
+              count % 256,
+              (count >> 8) % 256,
+              (count >> 16) % 256,
+              (count >> 24) % 256,
+            ]),
+            0,
+            4, // Write 4 bytes
+            54, // starts writing at byte 54 in the file
+          );
+        });
         handleSttClose(msg, currentCallEndTime);
         break;
     }
   });
    
-  
-  // const mediaStreamClient = WebSocket.createWebSocketStream(ws, { encoding: "utf-8" });
   // const mediaDecoderStreamClient = new Transform({
   //   transform: (chunk, encoding, callback) => {
   //     const msg = JSON.parse(chunk.toString('utf-8'));
@@ -84,61 +111,6 @@ function handleIncomingWSConnection(ws, req) {
   // recognizeStreamClient.on('close', (msg) => { handleSttClose(msg) });
   // recognizeStreamClient.on('data', (msg) => { handleSttData(msg) });
 }
-
-// function newCall() {
-//   currentCallStartTime = Date.now();
-//     uiUpdater.newCall(currentCallStartTime);
-// }
-
-// function endCall(wss) {
-//   wss.clients.forEach((client) => {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(
-//         JSON.stringify({
-//           event: "end-call",
-//           timestamp: currentCallStartTime,
-//         })
-//       );
-//     }
-//   });
-// }
-
-// const startWebSocketServer = (server) => {
-//   const wss = new WebSocket.Server({ server });
-
-  // wss.on("connection", function connection(ws) {
-  //   console.log("New Connection Initiated");
-
-  //   let recognizeStream = null;
-
-  //   ws.on("message", function incoming(message) {
-  //     const msg = JSON.parse(message);
-  //     switch (msg.event) {
-  //       case "connected":
-  //         console.log(`A new call has connected.`);
-  //         newCall(wss);
-  //         break;
-  //       case "start":
-  //         console.log(`Starting Media Stream ${msg.streamSid}`);
-  //         recognizeStream = client
-  //           .streamingRecognize(request)
-  //           .on("error", console.error)
-  //           .on("data", (data) => { handleSttData(data, wss); });
-  //         break;
-  //       case "media":
-  //         recognizeStream.write(msg.media.payload);
-  //         break;
-  //       case "stop":
-  //         console.log(`Call Has Ended`);
-  //         recognizeStream.destroy();
-  //         endCall(wss);
-  //         break;
-  //     }
-  //   });
-  // });
-
-//   return wss;
-// };
 
 async function handleSttData(msg) {
   if (containsTranscript(msg)) {
